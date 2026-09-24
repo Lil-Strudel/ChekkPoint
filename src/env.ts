f7a86591234f926f8020658de098285a2609cef2
Lil-Strudel <42657503+Lil-Strudel@users.noreply.github.com>
2026-09-23 19:34:25 -0600

    feat: install t3env

---
+++ src/env.ts
@@ -0,0 +1,11 @@
+import "@tanstack/solid-start/server-only";
+import { createEnv } from "@t3-oss/env-core";
+import * as z from "zod";
+
+export const env = createEnv({
+	server: {
+		DATABASE_URL: z.url(),
+	},
+	runtimeEnv: process.env,
+	emptyStringAsUndefined: true,
+});
